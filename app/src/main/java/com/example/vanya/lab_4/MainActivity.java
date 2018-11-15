538b1aa3d8f4536c023bf5fbbc4f8c3c0d1c91e0
Ваня Ряничев <vanyaryanichev@outlook.com>
2018-11-15 15:32:42 +0200

    BOOM! lab_4 is DONE!

--- app/src/main/java/com/example/vanya/lab_4/MainActivity.java
+++ app/src/main/java/com/example/vanya/lab_4/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.vanya.lab_4;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,18 +15,15 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    String[] currencies = {"USD", "EUR", "RUB"};
+    String[] currencies = {"UAH","USD", "EUR", "RUB"};
     Button button;
-    Spinner spinner;
-    Spinner spinner2;
-    public static Toast toast;
+    public static Spinner spinner;
+    public static Spinner spinner2;
     public static TextView textView;
     public static EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Toast toast = Toast.makeText(getApplicationContext(),
-                "JSON FAILED", Toast.LENGTH_SHORT);
-        Log.d("stroka", "KAKASHKA");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textView);
@@ -33,15 +32,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         spinner = findViewById(R.id.currenciesList);
         spinner2 = findViewById(R.id.spinner);
         editText = findViewById(R.id.editText2);
-        Spinner currenciesList = (Spinner) findViewById(R.id.currenciesList);
+        Spinner currenciesList =  findViewById(R.id.currenciesList);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, currencies);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         currenciesList.setAdapter(adapter);
-
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, currencies);
+        Spinner spinner = findViewById(R.id.spinner);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
 
 
     }
@@ -49,9 +47,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button:
-               receiveData process = new receiveData();
-               process.execute();
-
+                if(editText.getText().length() == 0)
+                {
+                    Toast.makeText(getApplicationContext(),"Fell the field!!!",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                else {
+                    receiveData process = new receiveData();
+                    process.execute();
+                }
                 break;
 
             default:
@@ -59,4 +64,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
+
+
 }
--- app/src/main/java/com/example/vanya/lab_4/receiveData.java
+++ app/src/main/java/com/example/vanya/lab_4/receiveData.java
@@ -1,95 +1,69 @@
 package com.example.vanya.lab_4;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Toast;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
 
 
-public class receiveData extends AsyncTask<Void,Void,Void>  {
+public class receiveData extends AsyncTask<Void, Void, Void> {
     String data = "";
-    String dataParsed;
-    String singleParsed = "";
-    String JS = "JSON FAILED";
-    String json;
+    String convertedSum;
     double result;
+    Currency EUR;
+    Currency USD;
+    Currency RUB;
+
     @Override
     protected Void doInBackground(Void... voids) {
 
 
-            try {
+        try {
 
-                URL url = new URL("http://resources.finance.ua/ru/public/currency-cash.json");
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                InputStream inputStream = httpURLConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
+            URL url = new URL("http://resources.finance.ua/ru/public/currency-cash.json");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
 
-                //JSONObject jsonObj = new JSONObject(jsonStr);
-
-
-                String line = "";
-                while (line != null) {
-                    line = bufferedReader.readLine();
-                    data = data + line;
-                }
-                JSONObject JO = new JSONObject(data);
-                JSONArray JA = JO.getJSONArray("organizations");
-                JSONObject organization = JA.getJSONObject(0);
-                JSONObject currency = organization.getJSONObject("currencies");
-                JSONObject EUR = currency.getJSONObject("EUR");
-                JSONObject USD = currency.getJSONObject("USD");
-                JSONObject RUB = currency.getJSONObject("RUB");
-                Currency EUR1 = new Currency(EUR.getDouble("ask"),EUR.getDouble("bid"));
-                Currency USD1 = new Currency(USD.getDouble("ask"),USD.getDouble("bid"));
-                Currency RUB1 = new Currency(RUB.getDouble("ask"),RUB.getDouble("bid"));
-                String eu = "EURO "+EUR1.getAsk()+" "+EUR1.getBid();
-                Log.e("EUR1",eu);
-                String us = "DOLLAR "+USD1.getAsk()+" "+USD1.getBid();
-                Log.e("USD1",us);
-                String ru = "RUB "+RUB1.getAsk()+" "+RUB1.getBid();
-                Log.e("RUB1",ru);
-                json = MainActivity.editText.getText().toString();
-                result = EUR1.ask * Double.parseDouble(json);
-                json = result+"";
-
-
-               /* for (int i = 0; i < JA.length(); i++) {
-                    JSONObject J = (JSONObject) JA.get(i);
-
-                    line = J.getString("ask");
-                    Log.e(i+"st currency",line);
-                    line = J.getString("bid");
-                    Log.e(i+"st currency",line);
-                    if (J.getString("title").equals("А-Банк")) {
-
-                        Log.e("A-BANK", "AVAILABLE");
-
-                    }
-                }*/
-
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            catch (JSONException e) {
-                e.printStackTrace();
+            String line = "";
+            while (line != null) {
+                line = bufferedReader.readLine();
+                data = data + line;
             }
 
+            // Я не знаю правильно ли я это сделал
+            // I don't know if this is right {
+            JSONObject JO = new JSONObject(data);
+            JSONArray JA = JO.getJSONArray("organizations");
+            JSONObject organization = JA.getJSONObject(0);
+            JSONObject currency = organization.getJSONObject("currencies");
+            JSONObject EURobj = currency.getJSONObject("EUR");
+            JSONObject USDobj = currency.getJSONObject("USD");
+            JSONObject RUBobj = currency.getJSONObject("RUB");
+            EUR = new Currency(EURobj.getDouble("ask"), EURobj.getDouble("bid"));
+            USD = new Currency(USDobj.getDouble("ask"), USDobj.getDouble("bid"));
+            RUB = new Currency(RUBobj.getDouble("ask"), RUBobj.getDouble("bid"));
+            // } But this is what i did
+            // Но я сделал это так
+
+            result = Convert(MainActivity.spinner.getSelectedItemPosition(), MainActivity.spinner2.getSelectedItemPosition());
+            convertedSum = String.format("%.2f", result);
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
 
         return null;
@@ -99,6 +73,56 @@ public class receiveData extends AsyncTask<Void,Void,Void>  {
     @Override
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
-        MainActivity.textView.setText(this.json);
+        MainActivity.textView.setText(this.convertedSum);
+    }
+
+    double Convert(int id1, int id2) {
+        switch (id1) {
+            case 0:
+                if (id2 == 0)
+                    return Double.parseDouble(MainActivity.editText.getText().toString());
+                if (id2 == 1)
+                    return Double.parseDouble(MainActivity.editText.getText().toString()) / USD.ask;
+                if (id2 == 2)
+                    return Double.parseDouble(MainActivity.editText.getText().toString()) / EUR.ask;
+                if (id2 == 3)
+                    return Double.parseDouble(MainActivity.editText.getText().toString()) / RUB.ask;
+                break;
+            case 1:
+                if (id2 == 0)
+                    return (Double.parseDouble(MainActivity.editText.getText().toString())) * USD.ask;
+                if (id2 == 1)
+                    return Double.parseDouble(MainActivity.editText.getText().toString());
+                if (id2 == 2)
+                    return (Double.parseDouble(MainActivity.editText.getText().toString())) * USD.ask / EUR.ask;
+                if (id2 == 3)
+                    return (Double.parseDouble(MainActivity.editText.getText().toString())) * USD.ask / RUB.ask;
+                break;
+
+            case 2:
+                if (id2 == 0)
+                    return Double.parseDouble(MainActivity.editText.getText().toString()) * EUR.ask;
+                if (id2 == 1)
+                    return Double.parseDouble(MainActivity.editText.getText().toString()) * EUR.ask / USD.ask;
+                if (id2 == 2)
+                    return Double.parseDouble(MainActivity.editText.getText().toString());
+                if (id2 == 3)
+                    return Double.parseDouble(MainActivity.editText.getText().toString()) * EUR.ask / RUB.ask;
+
+                break;
+            case 3:
+                if (id2 == 0)
+                    return Double.parseDouble(MainActivity.editText.getText().toString()) * RUB.ask;
+                if (id2 == 1)
+                    return Double.parseDouble(MainActivity.editText.getText().toString()) * RUB.ask / USD.ask;
+                if (id2 == 2)
+                    return Double.parseDouble(MainActivity.editText.getText().toString()) * RUB.ask / EUR.ask;
+                if (id2 == 3)
+                    return Double.parseDouble(MainActivity.editText.getText().toString());
+                break;
+        }
+        return 0;
     }
 }
+// P.S. My apologize to man who will try check this out
+// Мои извенения тому кто будет это проверять
